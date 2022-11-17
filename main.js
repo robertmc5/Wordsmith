dc2c4da02c1e9bae28be331890baab7b65e00a32
Robert McCarter <69922179+robertmc5@users.noreply.github.com>
2022-11-17 15:06:31 -0500

    feat: Handle multi-parameter API query panels

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 // Datamuse API url prefix used for site functionality
 const urlAPI = 'https://api.datamuse.com/words?';
 
-// DOM selectors
+// Main DOM selectors
 const wordInput = document.getElementById('input');
 const btnInput = document.getElementById('submit-btn');
 const display = document.getElementById('output');
 const panels = Array.from(document.getElementsByClassName('panel'));
+// Special panel DOM selectors
+const relStart = document.getElementById('relStart');
+const relEnd = document.getElementById('relEnd');
+const relWord = document.getElementById('relWord');
 
 // Specific functions of Datamuse API
-const queryType = 'rel_trg=';                                     /* Initally just one .. TODO expand to many */
+let queryType = '';
+let query2nd = '';
+let auxInput = '';
 
 // Async function to GET result suggestions from API
 const getSuggestions = async () => {
   let wordQuery = wordInput.value;
-  const endpoint = urlAPI + queryType + wordQuery;
+  const endpoint = urlAPI + queryType + wordQuery + query2nd + auxInput;
+  console.log('ENDPOINT ' + endpoint);                                        /* TEST */
   try {
     const response = await fetch(endpoint);
     if (response.ok) {
@@ -58,29 +65,50 @@ const renderResults = APIresponse => {
 
 // Select panel to choose word feature
 const selectPanel = (event) => {
+  let current = event.currentTarget;
   console.log('CLICK feature PANEL at ' + event.currentTarget);                                /* TEST */
   panels.forEach(panel => {
     panel.setAttribute('style', 'background-color: #44804C; color: #fbf05b;'); 
   });
-  event.currentTarget.setAttribute('style', 'background-color: #ABA104; color: #041e34;');        /* TODO */
+  current.setAttribute('style', 'background-color: #ABA104; color: #041e34;');        /* TODO */
+  queryType = current.getAttribute('data-feat');                    /* TODO - queryType via data-* */
+  query2nd = current.getAttribute('data-2nd');
+  if (query2nd === '&sp=*') auxInput = current.querySelector('select').value;
+  if (query2nd === '&sp=') auxInput = current.querySelector('select').value + '*';
+  if (query2nd === '&topics=') auxInput = current.querySelector('input').value;
+  if (!query2nd) {
+    query2nd = '';
+    auxInput = '';
+  }
+  console.log('query2nd at ' + query2nd + ' ' + auxInput);                                /* TEST */
 }
 
-// Clear past results in output display area
+// Clear past results in output display area then call API functionality
 const displayResults = (event) => {
   event.preventDefault();
   if (display.lastChild) {
     display.removeChild(display.lastChild);
   }
+  if (query2nd === '&topics=') auxInput = document.getElementById('relWord').value;           /* TEST */     
   getSuggestions();
 }
 
-// Register event listener on word feature panels
+// Register click event listener on word feature panels
 panels.forEach(panel => {
   panel.addEventListener('click', selectPanel);
 });
 
-// Register event listener on word query button
+// Register Enter key event listener on text input for panel
+const enterKey = (e) => {
+  if (e.key === 'Enter') {
+    auxInput = e.target.value;
+    btnInput.focus();
+  }
+}
+relWord.addEventListener('keydown', enterKey);
+
+// Register click event listener on word query button
 btnInput.addEventListener('click', displayResults);
 
 // Set default event for initially selected word feature panel
-document.getElementById('default').dispatchEvent(new Event("click"));
+document.getElementById('default').dispatchEvent(new Event('click'));
